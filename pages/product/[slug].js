ffc4e0190f6636ec431669d1de697d0d7f5cad06
prayudaprianggara <prayudaprianggara@gmail.com>
2023-01-10 22:00:44 +0700

    add useRouter

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,7 +8,7 @@ import { Store } from '../../utils/Store'
 
 export default function ProductScreen() {
   const { state, dispatch } = useContext(Store)
-
+  const router = useRouter()
   const { query } = useRouter()
   const { slug } = query
   const product = data.products.find((x) => x.slug === slug)
